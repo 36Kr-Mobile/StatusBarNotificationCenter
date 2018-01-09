a2c723f53e7fc1c4dc5546ff6e1e63b9eff4fc76
Eliah Snakin <ilya@nikans.com>
2018-01-09 18:10:04 +0300

    swift 4

--- Pod/Notification Center/BaseScrollLabel.swift
+++ Pod/Notification Center/BaseScrollLabel.swift
@@ -20,7 +20,7 @@ class BaseScrollLabel: UILabel {
     
     var fullWidth: CGFloat {
         guard let message = text else { return 0 }
-        return (message as NSString).size(attributes: [NSFontAttributeName : font]).width
+        return (message as NSString).size(withAttributes: [NSAttributedStringKey.font : font]).width
     }
     
     var scrollOffset: CGFloat {
